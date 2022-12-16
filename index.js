4277618ae427bde0df51c9e6f35a655574f07ca1
orbitadajogatina <100052508+orbitadajogatina@users.noreply.github.com>
2022-12-16 11:47:34 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+// Pergunta do Dia Bot v0.0.1 - criado pelo Enzo da Órbita da Jogatina
+// Código adaptado do discord.js Guide (https://discordjs.guide)
+
+"use strict";
+
+const fs = require('fs');
+const logError = require('./core/logError');
+const botWeb = require('express')();
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const supabase = require('@supabase/supabase-js');
+const database = supabase.createClient('https://onlntxblavzgsutdbson.supabase.co', process.env.SUPABASE_TOKEN);
+
+const bot = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildEmojisAndStickers,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages
+  ],
+  partials: [
+    Partials.Channel
+  ]
+});
+
+global.bot = bot;
+global.database = database;
+
+require('./core/commandsCore').deployCommands();
+require('./core/eventsCore').deployEvents();
+
+process.on('unhandledRejection', (reason, promise) => logError(reason, promise));
+process.on('uncaughtException', (reason, origin) => logError(reason, origin));
+
+botWeb.get('/', (req, res) => { 
+  res.send('❔📆🥰 - Pergunta do Dia no ar!');
+});
+
+botWeb.listen(3000);
+bot.login(process.env.DISCORD_TOKEN);
