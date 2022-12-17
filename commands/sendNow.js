391d63040ac05644e142ff6a9112c29a5f8dc9df
orbitadajogatina <100052508+orbitadajogatina@users.noreply.github.com>
2022-12-17 18:30:20 -0300

    v0.0.2.4

--- commands/sendNow.js
+++ commands/sendNow.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
 
 const properties = new SlashCommandBuilder()
 .setName('forçar-envio')
@@ -8,7 +8,7 @@ const properties = new SlashCommandBuilder()
 .setDMPermission(false);
 
 function execute (interaction) {
-  const userIsAdmin = interaction.user.id == '668199172276748328' || interaction.member.permissions.has('ADMINISTRATOR');
+  const userIsAdmin = interaction.user.id == '668199172276748328' || interaction.member.permissions.has([PermissionsBitField.Flags.Administrator]);
 
   if (userIsAdmin) {
     interaction.reply('**Tá bom.** Enviando pergunta agora.')
