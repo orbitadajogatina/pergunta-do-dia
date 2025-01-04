a7a11c2ad59ce6658e680489d66e7aaa6c9c8423
Ueslei Paim <uesleibros@gmail.com>
2025-01-04 01:04:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 
 const { initializeBot } = require('./app/bot');
 const { initializeWeb } = require('./app/web');
+const { initializeDatabase } = require('./app/database');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 (async () => {
     await initializeBot();
+    initializeDatabase()
     initializeWeb();
 })();
