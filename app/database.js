3d1440a008b4db0bdffa977792d052828122da86
Ueslei Paim <uesleibros@gmail.com>
2025-01-04 01:05:11 -0300

    Update database.js

--- app/database.js
+++ app/database.js
@@ -4,8 +4,8 @@ const { createClient } = require('@supabase/supabase-js');
 
 const database = createClient('https://onlntxblavzgsutdbson.supabase.co', process.env.SUPABASE_TOKEN);
 
-function declareDatabase() {
+function initializeDatabase() {
     global.database = database;
 }
 
-module.exports = { declareDatabase };
+module.exports = { initializeDatabase };
