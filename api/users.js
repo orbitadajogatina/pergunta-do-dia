15640270e196eb52ab6feebc4cd11d413fea20d4
orbitadajogatina <100052508+orbitadajogatina@users.noreply.github.com>
2025-01-05 18:41:58 -0300

    refactor: instead of /me endpoint should be /users/me

--- api/users.js
+++ api/users.js
@@ -1,6 +1,9 @@
 const { cleanUserData } = require('../utils/cleanUserData');
 
 async function get(req, res, authorization) {
+  const id = req.params.id?.toString();
+  if (id !== 'me') return res.status(404).send(`Invalid endpoint: ${req.params.endpoint}`);
+
   const discordUser = await bot.users.fetch(authorization.owner);
   const user = structuredClone(discordUser.toJSON());
   cleanUserData(user);
