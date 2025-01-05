d82ba607060a69af09952b223130940b07c7faae
orbitadajogatina <100052508+orbitadajogatina@users.noreply.github.com>
2025-01-05 18:35:50 -0300

    refactor: more explanation and format to /emojis

--- commands/emojisHelp.js
+++ commands/emojisHelp.js
@@ -5,20 +5,23 @@ const properties = new SlashCommandBuilder()
   .setDescription('Um guia de uso para emojis.');
 
 async function execute(interaction) {
-  await interaction.reply(`• Criar emojis usando imagens aleatórias da internet com base na opção
-Use \`$$\` ao invés do emoji, assim: \`$$ - Feijão carioca\`. Isso pesquisará por imagens de "Feijão carioca" na internet.
+  await interaction.reply(
+    `- Criar emojis usando imagens aleatórias da internet com base na opção
+    Use \`$$\` ao invés do emoji, assim: \`$$ - Feijão carioca\`. Isso pesquisará por imagens de "Feijão carioca" na internet e aleatoriamente uma será selecionada para ser o emoji.
 
-• Criar emojis usando imagens aleatórias da internet com base em termos de consulta
-Use \`$pesquisa$\` ao invés do emoji, assim: \`$salada de frutas$ - James\`. Isso pesquisará por imagens de "Salada de frutas" na internet.
+    - Criar emojis usando imagens aleatórias da internet com base em termos de consulta
+    Use \`$pesquisa$\` ao invés do emoji, assim: \`$salada de frutas$ - James\`. Isso pesquisará por imagens de “Salada de frutas” na internet e aleatoriamente uma será selecionada para ser o emoji.
 
-• Criar emojis usando link de uma imagem
-Use \`$link$\` ao invés do emoji, assim: \`$http://pudim.com.br/pudim.jpg$ - Pudim\`.
+    - Criar emojis usando link de uma imagem
+    Use \`$link$\` ao invés do emoji, assim: \`$http://pudim.com.br/pudim.jpg$ - Pudim\`. A imagem do URL será o emoji.
 
-• Enumerar automaticamente opções usando emojis (1 a 20)
-Use \`##\` ao invés do emoji, assim: \`## - Lápis\`.
+    - Enumerar automaticamente opções usando emojis (1 a 20)
+    Use \`##\` ao invés do emoji, assim: \`## - Lápis\`.
 
-• Usar emojis normais
-Adicione usando \`:nomedoemoji:\`, através do seu teclado ou \`Windows + .\`. Para emojis do servidor, é necessário informar o ID, [saiba mais](https://support.discord.com/hc/en-us/community/posts/360069335891/comments/1500000646661).`);
+    - Usar emojis normais
+    Adicione usando \`:nomedoemoji:\`, através do seu teclado ou \`Windows + .\`. Para emojis do servidor, é necessário informar o ID, [saiba mais](https://support.discord.com/hc/en-us/community/posts/360069335891/comments/1500000646661).
+     
+    > -# **DICA: ** Essa mesma formatação de pesquisa de imagens para criar emojis também pode ser utilizada para pesquisar imagens que acompanham perguntas.`);
 }
 
 module.exports = { properties, execute, id: 'emojisHelp' };
