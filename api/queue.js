cb4c17576837d0dd98804c322191c803b7c5e223
orbitadajogatina <100052508+orbitadajogatina@users.noreply.github.com>
2025-01-05 17:16:11 -0300

    style & refactor: add other details to user object

--- api/queue.js
+++ api/queue.js
@@ -1,26 +1,19 @@
+const { cleanUserData } = require('../utils/cleanUserData');
+
 async function get() {
   const { data: queue } = await database.rpc("get_queue");
-  const lastQuestionsAuthorsData = await database
-    .from("variables")
-    .select()
-    .eq("key", "lastQuestionsAuthors");
-  const lastQuestionsAuthors = JSON.parse(
-    lastQuestionsAuthorsData.data[0]?.value || "[]"
-  );
+  const lastQuestionsAuthorsData = await database.from("variables").select().eq("key", "lastQuestionsAuthors");
+  const lastQuestionsAuthors = JSON.parse(lastQuestionsAuthorsData.data[0]?.value || "[]");
 
   const queueFinalObject = await Promise.all(
     queue.map(async (authorId) => {
       const discordUser = await bot.users.fetch(authorId);
       const user = structuredClone(discordUser.toJSON());
-
-      const filteredUser = {
-        id: user.id,
-        username: user.username,
-      };
+      cleanUserData(user);
 
       return {
         alreadySent: lastQuestionsAuthors.includes(authorId),
-        author: filteredUser,
+        author: user,
       };
     })
   );
