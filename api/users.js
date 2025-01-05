dac0b15aa24aae1819163c97702ab48cea59fd53
orbitadajogatina <100052508+orbitadajogatina@users.noreply.github.com>
2025-01-05 18:46:03 -0300

    fix: double responding

--- api/users.js
+++ api/users.js
@@ -2,7 +2,7 @@ const { cleanUserData } = require('../utils/cleanUserData');
 
 async function get(req, res, authorization) {
   const id = req.params.id?.toString();
-  if (id !== 'me') return res.status(404).send(`Invalid endpoint: ${req.params.endpoint}`);
+  if (id !== 'me') throw { message: `Invalid endpoint: ${req.params.endpoint}`, status: 404 };
 
   const discordUser = await bot.users.fetch(authorization.owner);
   const user = structuredClone(discordUser.toJSON());
