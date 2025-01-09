a99bd1aa1f5553fbcfb593eaaa40070a47110efd
orbitadajogatina <100052508+orbitadajogatina@users.noreply.github.com>
2025-01-09 17:50:46 -0300

    fix: errors should be in json format

--- utils/resolveEndpoint.js
+++ utils/resolveEndpoint.js
@@ -5,13 +5,13 @@ const fs = require("fs");
 
 async function resolveEndpoint(req, res, endpoint, method, apiAuthorization) {
   const modulePath = path.resolve(__dirname, "../api", endpoint);
-  if (!fs.existsSync(`${modulePath}.js`)) return res.status(404).send(`Invalid endpoint: ${endpoint}`);
+  if (!fs.existsSync(`${modulePath}.js`)) return res.status(404).json({ error: `Invalid endpoint: ${endpoint}` });
 
   const authorization = await apiAuthorization(req.get("Authorization")?.match(/Bearer (.*)/)?.[1]);
   if (authorization === false) {
-    return res.status(401).send(`Invalid token. Please get your API key on Discord using the bot or through the website.`);
+    return res.status(401).json({ error: `Invalid token. Please get your API key on Discord using the bot or through the website.` });
   } else if (authorization === 'suspended') {
-    return res.status(403).send(`You are suspended. Please contact ${process.env.SUPER_ADMIN_NAME}.`);
+    return res.status(403).json({ error: `You are suspended. Please contact ${process.env.SUPER_ADMIN_NAME}.` });
   }
 
   try {
@@ -22,11 +22,11 @@ async function resolveEndpoint(req, res, endpoint, method, apiAuthorization) {
       const result = await module[method](req, res, authorization);
       return res.json(result);
     } else {
-      res.status(404).send(`${method.toUpperCase()} method not available for this endpoint: ${param}`);
+      res.status(404).json({ error: `${method.toUpperCase()} method not available for this endpoint: ${param}` });
     }
   } catch (error) {
     console.error(`Error loading module: ${endpoint}`, error);
-    res.status(error.status || 500).send(`Error: ${error.message}`);
+    res.status(error.status || 500).json({ error: `Error: ${error.message}` });
   }
 }
 
